5c27a35ae8b41b04b153a94b92d30fec3b90cff3
PAVEL <146110653+Chebyrashkin@users.noreply.github.com>
2026-05-17 01:30:08 +0500

    Create 4.2.cpp

---
+++ 4.2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+//Работает только для английских текстов
+
+int main() {
+    string s;
+    getline(cin, s);
+    int count = 0;
+    for (size_t i = 0; i < s.length(); ) {
+        if (s[i] == ' ') { i++; continue; }
+        size_t j = s.find(' ', i);
+        if (j == string::npos) j = s.length();
+        string word = s.substr(i, j - i);
+        if (word[0] == word[word.length() - 1]) count++;
+        i = j + 1;
+    }
+    cout << count << '\n';
+    return 0;
+}

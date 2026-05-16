cd4a11a91de5850137341ac70747747f2bd37a69
PAVEL <146110653+Chebyrashkin@users.noreply.github.com>
2026-05-17 01:55:11 +0500

    Create 7.1.cpp

---
+++ 7.1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+void swal(int &x, int &y) {
+    x = x + y;
+    y = x - y;
+    x = x - y;
+}
+
+int main() {
+    int a = 5, b = 3;
+    swal(a, b);
+    cout << a << ' ' << b << '\n';
+    return 0;
+}

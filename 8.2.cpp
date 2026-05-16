a7cdecaac7cdf44c68daab525a3009871799483c
PAVEL <146110653+Chebyrashkin@users.noreply.github.com>
2026-05-17 03:14:03 +0500

    Create 8.2.cpp

---
+++ 8.2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+double product(int n, int num = 2, int den = 1) {
+    if (n == 0) return 1;
+    double term = (double)num / den;
+    if (n % 2 == 0)
+        return term * product(n - 1, num, den + 2);
+    else
+        return term * product(n - 1, num + 2, den);
+}
+
+int main() {
+    int n;
+    cin >> n;
+    cout << product(n) << endl;
+    return 0;
+}
